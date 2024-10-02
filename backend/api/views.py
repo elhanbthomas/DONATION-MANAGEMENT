35fdbafa525688cd8b9deca409bb0fd1a556f9b6
elhanbthomas <elhanbthomas@gmail.com>
2024-10-02 13:31:14 +0530

    new commit

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World<h1>")
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
--- backend/center/models.py
+++ backend/center/models.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -12,18 +12,28 @@ class Center(models.Model):
     def __str__(self):
         return self.name
 
-class PhoneCenter(models.Model):   
-    CenterID = models.ForeignKey(Center,on_delete = models.SET_NULL,null= False)
-    PhoneNumber = phone = models.CharField(primary_key=True)
-
 class Volounteer(models.Model):
-    VolounteerID = models.CharField(max_length = 200)
-    name = models.ForeignKey(User.username,max_length = 200)  
-    city = models.CharField(max_length = 100)  
-    phone = models.CharField()
-    email = models.ForeignKey(User.email) 
-    def __str__(self):
-        return self.name 
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    Volounteer_id = models.CharField(primary_key=True, max_length=5)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    email = models.EmailField()
+    qualification = models.TextField()
+    designation = models.TextField()
+    Center_id = models.ForeignKey(Center, on_delete = models.CASCADE)
+
+    
+class PhoneCenter(models.Model):
+    Center_id = models.ForeignKey(Center, on_delete=models.CASCADE)
+    phonenumber = models.BigIntegerField()
+
+    
+
+class CenterShipping(models.Model):
+    ShippingID = models.SmallIntegerField(primary_key=True)
+    fromAdress = models.CharField(max_length=255)
+    toAdress = models.CharField(max_length=255)
+    
 
 class PhoneVolounteer(models.Model):
     VolounteerID = models.OneToOneField(Volounteer)
@@ -34,37 +44,14 @@ class VolounteerPickup(models.Model):
     timestamp = models.DateTimeField()
     VolounteerID = models.ManyToOneRel(Volounteer,null = False)
 
-class CenterRequest(models.Model):
-    RequestID = models.SmallIntegerField(primary_key=True)
-    quantity = models.IntegerField(null = False)
-    description = models.TextField(max_length=255)
 
-class CenterShipping(models.Model):
-    ShippingID = models.SmallIntegerField(primary_key=True)
-    fromAdress = models.CharField(max_length=255)
-    toAdress = models.CharField(max_length=255)
 
 class CenterReceive(models.Model):
     CenterReceiveID = models.IntegerField(primary_key=True)
     ShippingID = models.ManyToOneRel(CenterShipping, null = False)
     timestamp = models.DateTimeField(null = False)
 
-=======
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-class Center(models.Model):
-    host = models.OneToOneField(User,on_delete=models.CASCADE)
-    Center_id = models.CharField(primary_key=True, max_length=5)
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    isMain = models.BooleanField()
-    City = models.CharField(max_length=200)
-    State = models.CharField(max_length=200)
 
-class PhoneCenter(models.Model):
-    Center_id = models.ForeignKey(Center, on_delete=models.CASCADE)
-    phonenumber = models.BigIntegerField()
 
 class CenterShipment(models.Model):
     CenterShipment_id = models.CharField(max_length=5,primary_key=True)
@@ -78,17 +65,5 @@ class CenterRequest(models.Model):
     Center_id = models.ForeignKey(Center, on_delete=models.CASCADE)
     Shipment_id = models.ForeignKey(CenterShipment, on_delete=models.CASCADE)
 
-class Volounteer(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    Volounteer_id = models.CharField(primary_key=True, max_length=5)
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    email = models.EmailField()
-    qualification = models.TextField()
-    designation = models.TextField()
-    Center_id = models.ForeignKey(Center, on_delete = models.CASCADE)
 
-class PhoneVolounteer(models.Model):
-    Volounteer_id = models.ForeignKey(Volounteer, on_delete=models.CASCADE)
-    phonenumber = models.BigIntegerField()
->>>>>>> main
+
