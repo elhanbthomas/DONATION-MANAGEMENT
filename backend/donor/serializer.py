93b5fe2d98d093b5c158eba0aa46bcc6eab0c0f3
elhanbthomas <elhanbthomas@gmail.com>
2024-11-01 13:56:23 +0530

    update

--- backend/donor/serializer.py
+++ backend/donor/serializer.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
 from item.models import ItemPickup
+from center.serializers import ItemTypeSerializer, CenterListSerializer
+
 class DonorRequestSerializer(serializers.ModelSerializer):
-    
+    item_type = ItemTypeSerializer()
+    center = CenterListSerializer()
     class Meta:
         model = ItemPickup
         exclude = ['donor']
