e372fc5edc503c6d102d02a254536ff76097bb88
Shanwis <127328539+Shanwis@users.noreply.github.com>
2024-11-01 12:34:48 +0530

    Update models.py
    added Beneficiary shippment

--- backend/beneficiary/models.py
+++ backend/beneficiary/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.utils import timezone
+from item.models import ItemType
+from .models import BeneficiaryRequest  # Import BeneficiaryRequest if it's in the same app
+from center.models import Volunteer  # Adjust this import based on where the Volunteer model is located
 
 class Beneficiary(models.Model):
     B_id = models.CharField(max_length=4, primary_key=True)
@@ -23,5 +27,15 @@ class BeneficiaryRequest(models.Model):
     
     def __str__(self):
         return f"{self.beneficiary}: {self.item_type}"
-    
+
+class BeneficiaryShipment(models.Model):
+    bship_id = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    BR_id = models.ForeignKey(BeneficiaryRequest, on_delete=models.CASCADE, related_name="shipments")
+    V_id = models.ForeignKey(Volunteer, on_delete=models.CASCADE, related_name="deliveries")
+    isDelivered = models.BooleanField(default=False)
+    shipment_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Shipment {self.bship_id} for Request {self.BR_id_id} by Volunteer {self.V_id_id}"
+
     
