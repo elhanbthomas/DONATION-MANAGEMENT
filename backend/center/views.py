c0fd91ec5b1f5091b96e25f7ac96b128753c931a
elhanbthomas <elhanbthomas@gmail.com>
2024-11-04 12:04:13 +0530

    final

--- backend/center/views.py
+++ backend/center/views.py
@@ -288,15 +288,7 @@ def accept_request(request):
                 inventory.quantity -= center_request.quantity
                 inventory.save()
                 
-                to_center = center_request.center
-                count = Inventory.objects.count()
-                id = "I" + str(count+1)
-                inv, created = Inventory.objects.get_or_create(center=to_center, item_type=center_request.item_type, defaults={
-                    "quantity":center_request.quantity,
-                    })
-                if not created:
-                    inv.quantity += center_request.quantity
-                    inv.save()
+            
             else:
                 return Response(
                     {'error': 'Not enough inventory to fulfill the request'},
@@ -349,7 +341,8 @@ def mark_received(request):
                 try:
                     inventory, created = Inventory.objects.get_or_create(
                         center=volunteer.Center_id, 
-                        item_type=c_request.item_type
+                        item_type=c_request.item_type,
+                        defaults={'quantity':c_request.quantity}
                         )
                     if not created:
                         inventory.quantity += c_request.quantity
